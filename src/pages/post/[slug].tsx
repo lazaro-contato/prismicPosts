bf6f0068390174e0d93fd139b7d7974ce101605d
José Lázaro <contato.jlazaro@gmail.com>
2022-08-31 22:18:23 -0300

    add post render

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -3,6 +3,7 @@ import * as prismicH from '@prismicio/helpers';
 
 import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
+import Head from 'next/head';
 import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
@@ -30,9 +31,45 @@ interface PostProps {
   post: Post;
 }
 
-export default function Post({ page }) {
-  console.log(page);
-  return <div>vamos vamos</div>;
+export default function Post({ post }: PostProps) {
+  const postContent = post.data.content;
+
+  const renderPostContent = () => {
+    const contents = () =>
+      postContent.map(item => {
+        return (
+          <>
+            <h2>{item.heading}</h2>
+            <div>{item.body}</div>
+          </>
+        );
+      });
+
+    contents();
+  };
+  console.log(post);
+  return (
+    <>
+      <Head>
+        <title>{post.data.title}</title>
+      </Head>
+      <main className={styles.container}>
+        <img src={post.data.banner.url} alt={post.data.banner.url} />
+        <h1>{post.data.title}</h1>
+        <div>
+          <div>
+            <span>{post.first_publication_date}</span>
+          </div>
+          <div>
+            <span>{post.data.author}</span>
+          </div>
+          <div>
+            <span>4 Min</span>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
 
 export const getStaticProps: GetStaticProps = async ({
@@ -41,11 +78,21 @@ export const getStaticProps: GetStaticProps = async ({
 }) => {
   const client = createClient({ previewData });
   const uid = params.slug as string;
-  const page = await client.getByUID('post', uid);
+  const post = await client.getByUID('post', uid);
 
   return {
     props: {
-      page,
+      post: {
+        first_publication_date: post.first_publication_date,
+        data: {
+          title: post.data.title,
+          banner: {
+            url: post.data.banner.url,
+          },
+          author: post.data.author,
+          content: post.data.content,
+        },
+      },
     },
   };
 };
@@ -61,8 +108,3 @@ export const getStaticPaths: GetStaticPaths = async () => {
     fallback: true,
   };
 };
-
-// return {
-//   paths: pages.map(page => prismicH.asLink(page)),
-//   fallback: true,
-// };
